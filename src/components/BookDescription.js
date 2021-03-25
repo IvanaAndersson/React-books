21bc3478e5ef36bd831070e621ea615475bb9ce0
Ivana Andersson <ivana.p.kostova@gmail.com>
2021-03-25 09:24:54 +0200

    make go back button

--- src/components/BookDescription.js
+++ src/components/BookDescription.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import { useHistory } from "react-router-dom"
 import { useParams } from 'react-router'
 import useFetch from '../hooks/useFetch'
 
 const BookDescription = () => {
   const { id } = useParams()
-
+  const history = useHistory();
   const { data: book, error } = useFetch(`http://localhost:8000/books/${id}`)
+
+  const handleGoBack = () => {
+    history.go(-1)
+  }
   return (
     <div>
       { error && <div> {error} </div> }
       {
         book && (
         <section>  
-          <Link to="/">Back</Link>
+          <button className="btn" onClick={handleGoBack}>Back </button>
           <h1> Book { id } - {book.title} </h1>
           <p> {book.description} </p>
         </section>
